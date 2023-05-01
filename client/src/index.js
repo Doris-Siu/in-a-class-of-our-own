af4b9ee5cb38c66de056f33655b02fb48e55c7a5
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-01 18:57:45 +0100

    added dashboard route

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { DarkModeContextProvider } from "./context/darkModeContext";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
-		<App />
+		<DarkModeContextProvider>
+			<App />
+		</DarkModeContextProvider>
 	</BrowserRouter>
 );
---
+++ client/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import "./Dashboard.css";
+
+
+
+const Dashboard = () => {
+  return (
+    <h1>sfijfi</h1>
+  );
+};
+export default Dashboard;
