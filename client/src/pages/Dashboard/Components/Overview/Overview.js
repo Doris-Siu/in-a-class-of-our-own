b26a7a6599409b7972c081471b0bbfd7ab0a6759
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-02 19:46:45 +0100

    bug fix styling errors

--- client/src/pages/Dashboard/Components/Overview/Overview.js
+++ client/src/pages/Dashboard/Components/Overview/Overview.js
@@ -17,13 +17,6 @@ const Overview = () => {
 					<div></div>
 				</div>
 			</div>
-			<div className="card">
-				<div>
-					<h2>CYF Milstone</h2>
-					<p>You can find out how others doing:</p>
-					<div></div>
-				</div>
-			</div>
 		</div>
 	);
 };
