a9d9fefe9ab8f5daac76750000397addbce8247f
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-03 11:48:25 +0100

    added overview page

--- client/src/pages/Dashboard/Components/Overview/Overview.js
+++ client/src/pages/Dashboard/Components/Overview/Overview.js
@@ -3,18 +3,26 @@ import "./Overview.css";
 const Overview = () => {
 	return (
 		<div className="overview" >
-			<div className="card">
+			<div className="card overview-title">
+				<h4>YOUR CURRENT STATUS</h4>
 				<div>
-					<h2>CYF Milstone</h2>
-					<p>You can find out how others doing:</p>
-					<div></div>
+					<p>YOU ARE AT THE MILESTONE</p>
 				</div>
 			</div>
-			<div className="card">
-				<div>
-					<h2>User Milstone</h2>
-					<p>You can find out how others doing:</p>
-					<div></div>
+			<div className="chart-container">
+				<div className="card chart">
+					<div>
+						<h2>CYF Milstone</h2>
+						<p>You can find out how others doing:</p>
+						<div></div>
+					</div>
+				</div>
+				<div className="card chart">
+					<div>
+						<h2>User Milstone</h2>
+						<p>You can find out how others doing:</p>
+						<div></div>
+					</div>
 				</div>
 			</div>
 		</div>
