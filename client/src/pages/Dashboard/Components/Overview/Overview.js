878c4b98bc4a51eb004d8a6fdd1fb63915d90ab3
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-04 07:49:22 +0100

    chart js installed and charts added

--- client/src/pages/Dashboard/Components/Overview/Overview.js
+++ client/src/pages/Dashboard/Components/Overview/Overview.js
@@ -1,8 +1,46 @@
 import "./Overview.css";
-
+import "chart.js/auto";
+import { Chart } from "react-chartjs-2";
 const Overview = () => {
+	const traineeChartData = {
+		labels: ["Codewars", "# of PRs"],
+		datasets: [
+			{
+				data: [12, 19],
+				backgroundColor: [
+					"rgba(252, 3, 3)",
+					"rgb(15, 183, 26)",
+				],
+				borderColor: [
+					"rgba(252, 3, 3)",
+					"rgb(15, 183, 26)",
+				],
+				borderWidth: 1,
+			},
+		],
+	};
+	const cohortChartData = {
+		labels: ["Behind", "At", "Beyond"],
+		datasets: [
+			{
+				data: [3, 17, 1],
+				backgroundColor: [
+					"rgba(252, 3, 3)",
+					"rgb(15, 183, 26)",
+					"rgba(3, 82, 252)",
+				],
+				borderColor: [
+					"rgba(252, 3, 3)",
+					"rgb(15, 183, 26)",
+					"rgba(3, 82, 252)",
+				],
+				borderWidth: 1,
+			},
+		],
+	};
+
 	return (
-		<div className="overview" >
+		<div className="overview">
 			<div className="card overview-title">
 				<h5>YOUR CURRENT STATUS</h5>
 				<div>
@@ -14,14 +52,16 @@ const Overview = () => {
 					<div>
 						<h5>My Own Progress</h5>
 						<p>Github PRs & Codewars</p>
-						<div></div>
+						<Chart type="pie" data={traineeChartData} />
 					</div>
 				</div>
 				<div className="card chart-card">
 					<div>
 						<h5>My Cohort Progress</h5>
 						<p>Other trainees milestone</p>
-						<div></div>
+						<div>
+							<Chart type="pie" data={cohortChartData} />
+						</div>
 					</div>
 				</div>
 			</div>
