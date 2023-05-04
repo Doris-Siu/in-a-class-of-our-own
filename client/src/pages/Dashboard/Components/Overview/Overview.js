484d0e58d3634cf6f788a8ca689983a729f4199f
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-04 06:38:01 +0100

    added responsiveness for overview page

--- client/src/pages/Dashboard/Components/Overview/Overview.js
+++ client/src/pages/Dashboard/Components/Overview/Overview.js
@@ -4,20 +4,20 @@ const Overview = () => {
 	return (
 		<div className="overview" >
 			<div className="card overview-title">
-				<h4>YOUR CURRENT STATUS</h4>
+				<h5>YOUR CURRENT STATUS</h5>
 				<div>
 					<p>YOU ARE AT THE MILESTONE</p>
 				</div>
 			</div>
-			<div className="chart-container">
-				<div className="card chart">
+			<div className="charts-container">
+				<div className="card chart-card">
 					<div>
 						<h2>CYF Milstone</h2>
 						<p>You can find out how others doing:</p>
 						<div></div>
 					</div>
 				</div>
-				<div className="card chart">
+				<div className="card chart-card">
 					<div>
 						<h2>User Milstone</h2>
 						<p>You can find out how others doing:</p>
