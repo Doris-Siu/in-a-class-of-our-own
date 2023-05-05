7cebdc3465670372af8b9d86b603dcbe7c331bd6
Doris-Siu <doris.siu.my@gmail.com>
2023-05-05 15:58:49 +0100

    Update CSS

--- client/src/pages/Dashboard/Components/SetMilestone/SetMilestone.js
+++ client/src/pages/Dashboard/Components/SetMilestone/SetMilestone.js
@@ -1,53 +1,59 @@
+import "./SetMilestone.css";
+
 const SetMilestone = () => {
 	return (
-		<div>
+		<div className="page-container">
 			<div>
 				<p>
 					At CYF, your progress is tracked on a trainee tracker against
 					milestones set by the Director of Education. However, you are on your
-					own personal journey towards employment. and should really set your
+					own personal journey towards employment and should really set your
 					own milestones.
 				</p>
-				<p>
+				<p className="p1">
 					You can go to check `CYF Milestone` at the menu to have a reference on
 					how you should set your own milestone.
 				</p>
 			</div>
-			<div>
+			<div className="p2">
 				Please fill out the following form to create your new milestone.
 			</div>
-			<form>
-				<label htmlFor="milestoneSelect">Select the milestone</label>
-				<select id="milestoneSelect" required>
-					<option>Start</option>
-					<option>HTML-CSS Week 1 </option>
-					<option>JS1 Week 1</option>
-					<option>JS2 Week 1 </option>
-					<option>JS3 Week 3</option>
-					<option>React Week 2 </option>
-					<option>Node Week 2 </option>
-					<option>Databases Week 3 </option>
-					<option>Final Projects Week 2 </option>
-					<option>Post Graduation </option>
-				</select>
+			<form className="form-container">
 				<div>
-					<label htmlFor="factorInput">Add your factor</label>
+					<label htmlFor="milestoneSelect">Select the milestone:</label>
+					<select id="milestoneSelect" required>
+						<option>Start</option>
+						<option>HTML-CSS Week 1 </option>
+						<option>JS1 Week 1</option>
+						<option>JS2 Week 1 </option>
+						<option>JS3 Week 3</option>
+						<option>React Week 2 </option>
+						<option>Node Week 2 </option>
+						<option>Databases Week 3 </option>
+						<option>Final Projects Week 2 </option>
+						<option>Post Graduation </option>
+					</select>
+				</div>
+
+				<div>
+					<label htmlFor="factorInput">Add your factor:</label>
 					<input
 						type="text"
 						id="factorInput"
 						placeholder="Enter new factor"
 						required
-					/>
+					/>{" "}
+					<br />
 					<small id="factorHelp">
 						The factor entered must be quantified as integral value.
 					</small>
 				</div>
 				<div>
-					<label htmlFor="dateInput">Set due date</label>
+					<label htmlFor="dateInput">Set a due date:</label> <br />
 					<input type="date" id="dateInput" required />
 				</div>
 				<div>
-					<label htmlFor="valueInput">Set the goal value</label>
+					<label htmlFor="valueInput">Set the goal value:</label>
 					<input
 						type="number"
 						id="valueInput"
@@ -55,7 +61,9 @@ const SetMilestone = () => {
 						required
 					/>
 				</div>
-				<button type="submit">Set a milestone</button>
+				<button className="button" type="submit">
+					<span>Set milestone</span>
+				</button>
 			</form>
 		</div>
 	);
