24fd6e17a0e04ca6534273f99e3072a59ca3bdba
Alamin-eng <alaminruben@gmail.com>
2023-05-06 14:37:50 +0100

    added animation

--- client/src/pages/Dashboard/Components/CyfMilestone/CyfMilestone.js
+++ client/src/pages/Dashboard/Components/CyfMilestone/CyfMilestone.js
@@ -19,33 +19,45 @@ useEffect ( ()=> {
     fetchData();
 	},[]);
 	return (
-		<CDBContainer>
-      <h4 className="rounded text-center p-2 border bg-dark text-light" style={{ marginBottom:"-.1rem", letterSpacing:"5px" }}> CYF Milestone Table </h4>
-			<CDBTable hover bordered responsive>
-				<CDBTableHeader>
-					<tr className="bg-danger text-light font-weight-bolder">
-						<th className="pb-4 pt-4">Module</th>
-						<th className="pb-4 pt-4">Date</th>
-						<th className="pb-4 pt-4">Codewars Rank</th>
-						<th className="pb-4 pt-4">Github PRs</th>
-						<th className="pb-4 pt-4">Codewars Js Points</th>
-					</tr>
-				</CDBTableHeader>
-				<CDBTableBody>
-					{table.map((el, key) => {
-						return (
-							<tr key={key} className="bg-success text-light">
-								<td>{el.modulename}</td>
-								<td>{el.date.split("T")[0]}</td>
-								<td>{el.codewarsrank}</td>
-								<td>{el.githubprs}</td>
-								<td>{el.codewarsjspoints}</td>
-							</tr>
-						);
-					})}
-				</CDBTableBody>
-			</CDBTable>
-		</CDBContainer>
+			<CDBContainer>
+				<h4
+					className="rounded text-center p-3 border bg-dark text-light"
+					style={{
+						letterSpacing: "5px",
+						backgroundImage: `url(${"https://i.imgur.com/Yr32yur.gif"})`,
+            opacity:".97",
+						backgroundSize: "contain",
+						backgroundBlendMode: "lighten",
+					}}
+				>
+					{" "}
+					CYF Milestone Table{" "}
+				</h4>
+				<CDBTable hover bordered responsive>
+					<CDBTableHeader>
+						<tr className="bg-danger text-light font-weight-bolder">
+							<th className="pb-4 pt-4">Module</th>
+							<th className="pb-4 pt-4">Date</th>
+							<th className="pb-4 pt-4">Codewars Rank</th>
+							<th className="pb-4 pt-4">Github PRs</th>
+							<th className="pb-4 pt-4">Codewars Js Points</th>
+						</tr>
+					</CDBTableHeader>
+					<CDBTableBody>
+						{table.map((el, key) => {
+							return (
+								<tr key={key} className="bg-success text-light">
+									<td>{el.modulename}</td>
+									<td>{el.date.split("T")[0]}</td>
+									<td>{el.codewarsrank}</td>
+									<td>{el.githubprs}</td>
+									<td>{el.codewarsjspoints}</td>
+								</tr>
+							);
+						})}
+					</CDBTableBody>
+				</CDBTable>
+			</CDBContainer>
 	);
 };
 export default CyfMilestone;
