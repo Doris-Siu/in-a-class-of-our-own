c14b82a96f1318ed050076afdebe25da7b729223
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-08 12:31:19 +0100

    add form to register page

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,8 +1,51 @@
+import { useState } from "react";
 import "./Register.css";
 
 const Register = () => {
-  return (
-    <p>regis</p>
-  );
+	const [username, setUsername] = useState("");
+	const [github, setGithub] = useState("");
+	const [cohort, setCohort] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(
+			`Submitted form data: username=${username}, =${github}, =${cohort}`
+		);
+	};
+
+	return (
+		<div className="body">
+			<div>
+				<form onSubmit={handleSubmit}>
+					<label>
+						Github Account:
+						<input
+							type="text"
+							value={username}
+							onChange={(e) => setGithub(e.target.value)}
+						/>
+					</label>
+					<label>
+						User Name:
+						<input
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</label>
+					<label>
+						Cohort Name:
+						<input
+							type="text"
+							value={username}
+							onChange={(e) => setCohort(e.target.value)}
+						/>
+					</label>
+					<button type="submit">Register</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 export default Register;
