b3ef0a59b50822396aa070a333ad09603c53fed4
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-19 10:47:38 +0100

    fix bug context

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AppProvider } from "./Components/context";
+import { AppProvider } from "./Components/context.js";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
